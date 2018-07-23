0f538ab90b24bd53df7e0964cb7a5a42f52cb414
117503445 <t117503445@gmail.com>
2018-07-23 14:23:08 +0800

    Initial commit

---
+++ Demo1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Spire.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(@"C:\Users\117503445\Desktop\答案4.pdf");
+            doc.SaveToFile(@"C:\Users\117503445\Desktop\4.doc", FileFormat.DOC);
+            //System.Diagnostics.Process.Start("图文版丽江旅游攻略大全.doc");
+            Console.ReadLine();
+        }
+    }
+}
