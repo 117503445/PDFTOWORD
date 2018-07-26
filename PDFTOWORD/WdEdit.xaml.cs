2fa42c888a3923180c87437d34841cdfbd9adfef
117503445 <t117503445@gmail.com>
2018-07-26 23:03:13 +0800

    1.0

--- PDFTOWORD/WdEdit.xaml.cs
+++ PDFTOWORD/WdEdit.xaml.cs
@@ -16,7 +16,8 @@ using System.IO;
 using System.Windows.Threading;
 using TLib.Software;
 using System.Collections.ObjectModel;
-
+using TLib.UI.WPF_MessageBox;
+using Rec = System.Windows.Shapes.Rectangle;
 namespace PDFTOWORD
 {
     /// <summary>
@@ -24,19 +25,57 @@ namespace PDFTOWORD
     /// </summary>
     public partial class WdEdit : Window
     {
+
+        /// <summary>
+        /// 释放Bitmap
+        /// </summary>
+        ~WdEdit()
+        {
+            bitmap?.Dispose();
+        }
         /// <summary>
         /// 要操作的bitmap
         /// </summary>
         private Bitmap bitmap;
-
         private System.Drawing.Size bpSize;
-
         /// <summary>
         /// 维护点集
         /// </summary>
         public ObservableCollection<TPoint> Tps = new ObservableCollection<TPoint>();
+        private ObservableCollection<TPoint> LoadTps()
+        {
+            ObservableCollection<TPoint> p = new ObservableCollection<TPoint>();
+            if (!File.Exists(File_tpstxt))
+            {
+                return p;
+            }
+            var s = File.ReadAllText(File_tpstxt, Encoding.UTF8);
+            string[] ss = s.Split(';');
+            foreach (var item in ss)
+            {
+                var sss = item.Split(',');
+                if (sss.Length == 2)
+                {
+                    TPoint point = new TPoint(double.Parse(sss[0]), double.Parse(sss[1]));
+                    p.Add(point);
+                }
+            }
+            return p;
+        }
+        private void SaveTps()
+        {
+            Console.WriteLine("Save");
+            //File.Create(Dir_tpsTXT);
+            string s = "";
+            foreach (var item in Tps)
+            {
+                s += item.X + "," + item.Y + ";";
+            }
+            File.WriteAllText(File_tpstxt, s, Encoding.UTF8);
+        }
         public string Dir_WorkPlace { get; set; }
-        private string Dir_tpsTXT { get { return Dir_WorkPlace + "tps.txt"; } }
+        private string File_tpstxt { get { return Dir_WorkPlace + "tps.txt"; } }
+
         public WdEdit(string dir_WorkPlace)
         {
             string file_img = dir_WorkPlace + "pdf.jpg";
@@ -53,7 +92,7 @@ namespace PDFTOWORD
         {
 
             var i = new List<string> { "Tps" };
-            if (File.Exists(Dir_tpsTXT))
+            if (File.Exists(File_tpstxt))
             {
                 Tps = LoadTps();
             }
@@ -61,46 +100,12 @@ namespace PDFTOWORD
             {
                 SaveTps();
             };
-            UpdateEllipse();
+            UpdateEpsAndRs();
         }
-
-        private ObservableCollection<TPoint> LoadTps()
-        {
-            ObservableCollection<TPoint> p = new ObservableCollection<TPoint>();
-            if (!File.Exists(Dir_tpsTXT))
-            {
-                return p;
-            }
-            var s = File.ReadAllText(Dir_tpsTXT, Encoding.UTF8);
-            string[] ss = s.Split(';');
-            foreach (var item in ss)
-            {
-                var sss = item.Split(',');
-                if (sss.Length == 2)
-                {
-                    TPoint point = new TPoint(double.Parse(sss[0]), double.Parse(sss[1]));
-                    p.Add(point);
-                }
-            }
-            return p;
-        }
-
-        private void SaveTps()
-        {
-            //File.Create(Dir_tpsTXT);
-            string s = "";
-            foreach (var item in Tps)
-            {
-                s += item.X + "," + item.Y + ";";
-            }
-            File.WriteAllText(Dir_tpsTXT, s, Encoding.UTF8);
-        }
-
         private void Img_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
-
         /// <summary>
         /// 使ScrollViewer支持横向滚动
         /// </summary>
@@ -123,16 +128,16 @@ namespace PDFTOWORD
             }
             e.Handled = true;
         }
-
         private async void BtnSave_Click(object sender, RoutedEventArgs e)
         {
+            if (Tps.Count % 2 != 0)
+            {
+                await WdMessageBox.Display("消息", "请选中偶数个点", "", "", "知道了");
+                return;
+            }
             await Task.Run(() =>
             {
-                if (Tps.Count % 2 != 0)
-                {
-                    Console.WriteLine("点不对应");
-                    return;
-                }
+
                 TbInfo.Dispatcher.Invoke(() =>
                 {
                     TbInfo.Text = "切割图片中";
@@ -156,7 +161,7 @@ namespace PDFTOWORD
                 {
                     TbInfo.Text = "生成DOC中";
                 });
-                WordHelper.WriteWord($@"{App.Dir_Desktop}1.doc", dir_pic);
+                WordHelper.WriteWord($@"{App.Dir_Desktop}{TimeStamp.Now}.doc", dir_pic);
                 TbInfo.Dispatcher.Invoke(() =>
                 {
                     TbInfo.Text = "运行中";
@@ -164,12 +169,25 @@ namespace PDFTOWORD
             });
             BtnSave.IsEnabled = true;
         }
-
+        private async void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var i = await WdMessageBox.Display("消息", "真的要删除所有点吗?不过会留下备份的.", "", "取消", "删除");
+            if (i == 2)
+            {
+                File.Copy(File_tpstxt, File_tpstxt + TimeStamp.Now);
+                Tps = new ObservableCollection<TPoint>();
+                SaveTps();
+                Tps.CollectionChanged += (sd, arg) =>
+                {
+                    SaveTps();
+                };
+                UpdateEpsAndRs();
+            }
+        }
         private void BtnUndo_Click(object sender, RoutedEventArgs e)
         {
-
+            RemoveAPoint();
         }
-
         private void G_MouseDown(object sender, MouseButtonEventArgs e)
         {
             //Console.WriteLine("!");
@@ -191,38 +209,103 @@ namespace PDFTOWORD
             //l.Stroke = System.Windows.Media.Brushes.LightSteelBlue;
             //l.StrokeThickness = 10;
             //Panel.SetZIndex(l, 2);
-            Tps.Add(new TPoint(e.GetPosition(Img).X / Img.ActualWidth, e.GetPosition(Img).Y / Img.ActualHeight));
-            UpdateEllipse();
+            if (e.RightButton == MouseButtonState.Released)//左键
+            {
+                double x = e.GetPosition(Img).X / Img.ActualWidth;
+                double y = e.GetPosition(Img).Y / Img.ActualHeight;
+
+                bool isLegal = true;
+                if (Tps.Count > 0)
+                {
+                    isLegal = x > Tps.Last().X && y > Tps.Last().Y;
+                }
+
+                if (Tps.Count % 2 == 0 || (isLegal))
+                {
+                    Tps.Add(new TPoint(x, y));
+                }
+            }
+            else
+            {//右键
+                RemoveAPoint();
+            }
+
+            UpdateEpsAndRs();
+        }
+        private void G_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateEpsAndRs();
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            bitmap?.Dispose();
+        }
+
+        private void RemoveAPoint()
+        {
+            if (Tps.Count > 0)
+            {
+                Tps.RemoveAt(Tps.Count - 1);
+                UpdateEpsAndRs();
+            }
+            else
+            {
+                WdMessageBox.Display("消息", "无法再撤销了");
+            }
         }
         private List<Ellipse> eps = new List<Ellipse>();
-        private void UpdateEllipse()
+        private List<Rec> rs = new List<Rec>();
+        private void UpdateEpsAndRs()
         {
             for (int i = 0; i < eps.Count; i++)
             {
                 G.Children.Remove(eps[i]);
             }
-            for (int i = 0; i < Tps.Count; i++)
+            for (int i = 0; i < rs.Count; i++)
             {
-                Ellipse ep = new Ellipse();
-                ep.Visibility = Visibility.Visible;
-
-                ep.Margin = new Thickness(Tps[i].X * Img.ActualWidth, Tps[i].Y * Img.ActualHeight, bpSize.Width - (Tps[i].X * Img.ActualWidth) + 30, Img.ActualHeight - (Tps[i].Y * Img.ActualHeight) - 5);
-                //Img.ActualHeight- e.GetPosition(Img).Y+2
-                //ep.Width = 200;
-                //ep.Height = 200;
-                ep.Stroke = new SolidColorBrush(Colors.Red);
-                ep.StrokeThickness = 3;
-                ep.Fill = new SolidColorBrush(Colors.Red);
+                G.Children.Remove(rs[i]);
+            }
+            for (int i = 0; i < Tps.Count; i+=2)
+            {
+                Ellipse ep = new Ellipse
+                {
+                    Visibility = Visibility.Visible,
+                    Margin = new Thickness(Tps[i].X * Img.ActualWidth, Tps[i].Y * Img.ActualHeight, bpSize.Width - (Tps[i].X * Img.ActualWidth) + 30, Img.ActualHeight - (Tps[i].Y * Img.ActualHeight) - 5),
+                    //Img.ActualHeight- e.GetPosition(Img).Y+2
+                    //ep.Width = 200;
+                    //ep.Height = 200;
+                    Stroke = new SolidColorBrush(Colors.Red),
+                    StrokeThickness = 3,
+                    Fill = new SolidColorBrush(Colors.Red)
+                };
                 G.Children.Add(ep);
                 Panel.SetZIndex(ep, 2);
                 eps.Add(ep);
             }
+
+            for (int i = 0; i < Tps.Count; i += 2)
+            {
+                if (i + 1 < Tps.Count)
+                {
+                    Rec r = new Rec
+                    {
+                        Fill = new SolidColorBrush(System.Windows.Media.Color.FromArgb(0, 0, 0, 0)),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        Stroke = new SolidColorBrush(Colors.Red),
+                        StrokeThickness = 3,
+                        Margin = new Thickness(Tps[i].X * Img.ActualWidth, Tps[i].Y * Img.ActualHeight, 0, 0),
+                        Width = (Tps[i + 1].X - Tps[i].X) * Img.ActualWidth,
+                        Height = (Tps[i + 1].Y - Tps[i].Y) * Img.ActualHeight
+                    };
+                    G.Children.Add(r);
+                    Panel.SetZIndex(r, 2);
+                    rs.Add(r);
+
+                }
+            }
         }
 
-        private void G_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            UpdateEllipse();
-        }
     }
 
 }
